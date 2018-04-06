5e3a88b8912c58959c58ab4504c347aab99d250f
Kaushik Vecha <30440826+kaushikvecha@users.noreply.github.com>
2018-04-06 06:17:59 -0500

    Add files via upload

--- Realtor/src/AnagramFinder.java
+++ Realtor/src/AnagramFinder.java
@@ -11,6 +11,8 @@ import java.util.Scanner;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class AnagramFinder {
 	public static void main(String[] args) throws InterruptedException, IOException {
@@ -40,26 +42,31 @@ public class AnagramFinder {
 			Scanner s = new Scanner(System.in);
 			if (s.hasNext()) {
 				String input = s.next();
+				Pattern p = Pattern.compile("[a-zA-Z]");
+				Matcher m = p.matcher(input);
 				input = input.toLowerCase();
-				if (!input.equalsIgnoreCase("exit")) {
-					long sTime = System.nanoTime();
-					ExecutorService e = threadPool(dictionaryData, input);
-					long eTime = System.nanoTime();
-					if (e.isTerminated()) {
-						if (AnagramAlgo.l.size() > 0) {
-							System.out.println(AnagramAlgo.l.size() + " Anagrams found for " + input + " in "
-									+ (eTime - sTime) / 1000000 + "ms");
-							printingWordsByAppendingComma();
-						} else {
-							System.out.print(
-									"No Anagrams found for " + input + " in " + (eTime - sTime) / 1000000 + "ms");
+				if (m.find()) {
+					if (!input.equalsIgnoreCase("exit")) {
+						long sTime = System.nanoTime();
+						ExecutorService e = threadPool(dictionaryData, input);
+						long eTime = System.nanoTime();
+						if (e.isTerminated()) {
+							if (AnagramAlgo.l.size() > 0) {
+								System.out.println(AnagramAlgo.l.size() + " Anagrams found for " + input + " in "
+										+ (eTime - sTime) / 1000000 + "ms");
+								printingWordsByAppendingComma();
+							} else {
+								System.out.print(
+										"No Anagrams found for " + input + " in " + (eTime - sTime) / 1000000 + "ms");
+							}
+							AnagramAlgo.l.clear();
 						}
-						AnagramAlgo.l.clear();
+					} else {
+						s.close();
+						break;
 					}
-				} else {
-					s.close();
-					break;
-				}
+				} else
+					System.out.println("Try giving only alphabets");
 			}
 		}
 	}
@@ -86,7 +93,8 @@ public class AnagramFinder {
 		return e;
 	}
 
-	private static void loadFile(BufferedReader br, HashMap<Integer, ArrayList<String>> dictionaryData) throws IOException {
+	private static void loadFile(BufferedReader br, HashMap<Integer, ArrayList<String>> dictionaryData)
+			throws IOException {
 		String st;
 		ArrayList<String> list;
 		while ((st = br.readLine()) != null) {
@@ -120,17 +128,23 @@ class AnagramAlgo implements Runnable {
 	}
 
 	public boolean isAnagram(String s, String t) {
-		int[] alpha = new int[26];
-		if (s.length() != t.length())
-			return false;
-		for (int i = 0; i < s.length(); i++)
-			alpha[s.charAt(i) - 'a']++;
-		for (int i = 0; i < t.length(); i++)
-			alpha[t.charAt(i) - 'a']--;
-		for (int i : alpha)
-			if (i != 0)
+		try {
+			int[] alpha = new int[26];
+			if (s.length() != t.length())
 				return false;
-		return true;
+			for (int i = 0; i < s.length(); i++)
+				alpha[s.charAt(i) - 'a']++;
+			for (int i = 0; i < t.length(); i++)
+				alpha[t.charAt(i) - 'a']--;
+			for (int i : alpha)
+				if (i != 0)
+					return false;
+			return true;
+		} catch (ArrayIndexOutOfBoundsException e) {
+			System.out.println("Please provide only alphabets in input");
+			System.exit(1);
+			return false;
+		}
 	}
 
 }
